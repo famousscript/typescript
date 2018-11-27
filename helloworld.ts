3b8f2d0523241fedfd312f82e2234a2e58d134a2
Ghanshyam pandey <44994843+famousscript@users.noreply.github.com>
2018-11-27 10:24:37 +0530

    Hello World TypeScript
    Hello World TypeScript

---
+++ helloworld.ts
@@ -0,0 +1,20 @@
+var message:string = "Hello World!" 
+console.log(message)
+
+/* Instruction  how to run the type script code 
+
+1. Go to directory path in node command prompt.
+2. then type below command.
+3. 
+
+# Locally in your project
+npm install -D ts-node
+npm install -D typescript
+
+# Or globally (not recommended)
+npm install -g ts-node
+npm install -g typescript:
+
+4. Then run : ts-node helloword.ts // your file name with .ts
+
+*/
